fb99591339c614ada0f05da25db22681b253148e
Noah Schill <noahrschill@gmail.com>
2024-09-21 16:09:46 -0700

    Pygwalker

---
+++ src/frontend/pages/PYGWalker.py
@@ -0,0 +1,14 @@
+# """Display duckdb data using pygwalker."""
+
+# import pandas as pd
+# import streamlit as st
+# from duckdb_tools import get_tables
+# from pygwalker.api.streamlit import StreamlitRenderer
+
+# tables = get_tables()
+# tables
+# table = st.selectbox("Select Table", tables)
+# if table:
+#     pyg_app = StreamlitRenderer(df)
+
+#     pyg_app.explorer()
